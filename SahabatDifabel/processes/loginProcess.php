4028de56cef8c91400d9cd951e76e3c6f5c00985
apexmoxxy <103055171+apexmoxxy@users.noreply.github.com>
2022-12-22 06:27:23 +0700

    Add files via upload

--- SahabatDifabel/processes/loginProcess.php
+++ SahabatDifabel/processes/loginProcess.php
@@ -1,7 +1,10 @@
 <?php
 require('../classes/userClass.php');
+require('../classes/database.php');
 
 $logForm = $_POST;
+
+$database = new Database("localhost","sahabatdifabel","root","");
 
 echo "<pre>";
 var_dump($logForm);
--- SahabatDifabel/processes/registerProcess.php
+++ SahabatDifabel/processes/registerProcess.php
@@ -1,14 +1,21 @@
 <?php
 require('../classes/userClass.php');
+require('../classes/database.php');
 
 $regForm = $_POST;
+
+$database = new Database("localhost","sahabatdifabel","root","");
 
 $user = new User($regForm['username'],$regForm['password']);
 echo "Username: " .$user->getUsername()." is created with Password: " .$user->getPassword();
 
+/*
 echo "<pre>";
 var_dump($user);
 echo "</pre>";
+*/
+
+$database->createUser($user);
 
 header("Location: ../home.php");
 exit();
